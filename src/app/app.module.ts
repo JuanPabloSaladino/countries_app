3f1469ac25a4d8d3de7be61e26b91dce919bb634
JuanPabloSaladino <juanpablosaladino@gmail.com>
2021-09-15 16:24:02 -0300

    Add AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppComponent } from './app.component';
 import { CountriesModule } from './countries/countries.module';
 import { SharedModule } from './shared/shared.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     CountriesModule,
     SharedModule
   ],
